8d8d5ef1ffd0f6251b33e1c477f799c2dfe40008
daple <daple0803@gmail.com>
2023-05-16 21:50:22 +0700

    six commit

--- controller/order.js
+++ controller/order.js
@@ -89,9 +89,9 @@ export const processOrder = asyncError(async (req, res, next) => {
     const order = await Order.findById(req.params.id)
     if (!order) return next(new ErrorHandler("Không tìm thấy hóa đơn!", 404))
 
-    if (order.orderStatus === "Đợi xác nhận") order.orderStatus = "Đã xác nhận"
-    else if (order.orderStatus === "Đã xác nhận") {
-        order.orderStatus = "Đã được vận chuyển"
+    if (order.orderStatus === "Preparing") order.orderStatus = "Shipped"
+    else if (order.orderStatus === "Shipped") {
+        order.orderStatus = "Delivered"
         order.deliveredAt = new Date(Date.now())
     } else return next(new ErrorHandler("Đơn đã được vận chuyển!", 400))
 
